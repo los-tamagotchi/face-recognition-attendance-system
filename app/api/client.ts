e11e06ff934dfccfa1099957b86fc30c2b4cebc9
PolvosMagicos <73202603+PolvosMagicos@users.noreply.github.com>
2024-06-26 09:45:14 -0500

    commit

--- app/api/client.ts
+++ app/api/client.ts
@@ -8,19 +8,22 @@ export const client = axios.create({
   },
 });
 
+// Type for status object
+
 export type Status = {
   active: boolean;
   remainingTime: string;
 };
 
 // Util for POST requests
+
+export const useBaseMutation = <TVariables, TData>(
+  props: UseMutationOptions<TData, string, TVariables, any>,
+) => useMutation(props);
+
 /* 
 - TData: The type of the data that the mutation will return.
 - string: The type of the error, which in this case is a string.
 - TVariables: The type of the variables that will be passed to the mutation function.
 - any: The type of the context, which is typically used for optimistically updating the UI before the mutation is confirmed. 
 */
-
-export const useBaseMutation = <TVariables, TData>(
-  props: UseMutationOptions<TData, string, TVariables, any>,
-) => useMutation(props);
