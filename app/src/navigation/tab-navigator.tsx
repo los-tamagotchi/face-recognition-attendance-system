84bc7e668922f971772f07dbacc1a02c0544abac
damarizz <damaris.delcarpio@unmsm.edu.pe>
2024-07-03 13:10:55 -0500

    merge conflict

--- app/src/navigation/tab-navigator.tsx
+++ app/src/navigation/tab-navigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/home/home-screen';
+import Attendance from '../screens/home/attendance';
 import { VideoStream } from '../screens/streaming/streaming-screen';
 
 const Tab = createBottomTabNavigator();
@@ -10,6 +11,7 @@ const TabBarNavigator = () => {
   return (
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Records" component={Attendance} />
         <Tab.Screen name="Stream" component={VideoStream} />
       </Tab.Navigator>
   );
